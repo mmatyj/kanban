4fd6c677a6e045edae15a9b2e0abc7e58ac26ee3
Malgorzata Matyjanka <m.matyjanka@gmail.com>
2019-06-30 23:07:14 +0100

    finish kanban app

--- js/Card.js
+++ js/Card.js
@@ -14,9 +14,6 @@ function Card(id, name) {
         if (event.target.classList.contains('btn-delete-card')) {
             self.removeCard();
         }
-        if (event.target.classList.contains('edit-card')) {
-            self.editCard();
-        }
     });
 }
 Card.prototype = {
@@ -34,28 +31,5 @@ Card.prototype = {
                 self.element.parentNode.removeChild(self.element);
             })
     },
-    editCard: function () {
-        var self = this;
-        var newCardName = prompt("Enter a new name of the card");
-        event.preventDefault();
 
-        var data = new FormData();
-        data.append('column.id', self.id)
-        data.append('cards.name', newCardName);
-        data.append('cards.bootcamp_kanban_column_id', self.id);
-
-
-
-        fetch(prefix + baseUrl + '/card/' + self.id, {
-                method: 'PUT',
-                headers: myHeaders,
-                body: data
-            })
-            .then(function (resp) {
-                return resp.json();
-            })
-            .then(function (resp) {
-                self.name = newCardName;
-            });
-    }
 }
